41dc12e01c785b41c079711d4e44337447ff236d
Alex Heritier <alex.heritier@gmail.com>
2016-11-16 02:50:38 -0800

    implemented the cell scoring and the matrix traversal

--- main.hs
+++ main.hs
@@ -1,4 +1,5 @@
 import System.IO
+import Debug.Trace
 
 
 -- Cell data
@@ -11,7 +12,7 @@ leftCell :: Int -> Cell     -- left pointing cell
 leftCell score = ('-', score)
 
 diagonalCell :: Int -> Cell     -- up-left pointing cell
-diagonalCell score = ('\\', score)
+diagonalCell score = ('d', score)
 
 upCell :: Int -> Cell   -- upwards pointing cell
 upCell score = ('|', score)
@@ -21,23 +22,74 @@ upCell score = ('|', score)
 type Matrix = [[Cell]]  -- represents a 2D grid of Cells
 
 emptyMatrix :: Int -> Int -> Matrix
-emptyMatrix rows cols = [[emptyCell | _ <- [0..cols]] | _ <- [0..rows]]
+emptyMatrix rows cols = [[emptyCell | _ <- [0..cols-1]] | _ <- [0..rows-1]]
 
-setCellInRow :: Int -> Cell -> [Cell] -> [Cell]
-setCellInRow col val (a:as)
+_setCellInRow :: Int -> Cell -> [Cell] -> [Cell]
+_setCellInRow col val (a:as)
     | col == 0 = val:as
-    | otherwise = a:(setCellInRow (col - 1) val as)
+    | otherwise = a:(_setCellInRow (col - 1) val as)
 
 setCell :: Int -> Int -> Cell -> Matrix -> Matrix
+setCell row col val [(a:as)]
+    | row == 0 = [_setCellInRow col val (a:as)]
+    | otherwise = [(a:as)]
 setCell row col val ((a:as) : (b:bs))
     | row < 0 || col < 0 = ((a:as) : (b:bs))
-    | row == 0 = (setCellInRow col val (a:as)):(b:bs)
+    | row == 0 = newRow:(b:bs)
     | otherwise = (a:as):(setCell (row - 1) col val (b:bs))
+    where newRow = _setCellInRow col val (a:as)
+
+getCell :: Int -> Int -> Matrix -> Cell
+getCell row col m
+    | row < 0 || col < 0 = trace ("\nGET CELL\ti: " ++ show row ++ "\tj: " ++ show col) emptyCell
+    | row == length m || col == length (head m) = error "getCell: Index out of bounds"
+    | otherwise = (m !! row) !! col
 
 
 -- Algorithm
+type ScoringData = (Cell, Cell, Cell, Char, Char)
+mapInd :: Int -> Int -> (a -> Int -> b) -> [a] -> [b]
+mapInd rows cols f m = zipWith f m [0..]
+
+mapIndInner :: (Int -> Int -> b) -> [a] -> Int -> [b]
+mapIndInner f m index = zipWith f (cycle [index]) [0..(length m)-1]
+
+_mapMatrix :: Int -> Int -> (Matrix -> String -> String -> Int -> Int -> Cell) -> String -> String -> Matrix -> Matrix
+_mapMatrix rows cols f s1 s2 m = mapInd rows cols (mapIndInner $ scoreCell m s1 s2) m
+
+mapMatrix :: (Matrix -> String -> String -> Int -> Int -> Cell) -> String -> String -> Matrix -> Matrix
+mapMatrix f s1 s2 m = _mapMatrix (length m) (length $ head m) f s1 s2 m
+
+getResult :: Matrix -> String -> String -> [String]
+getResult m seq1 seq2 = [seq1, seq2]
+
+scoreCell :: Matrix -> String -> String -> Int -> Int -> Cell
+scoreCell m s1 s2 i j
+    | match > delete && match > insert = (diagonalCell match)
+    | delete > match && delete > insert = (upCell delete)
+    | insert > match && insert > delete = (leftCell insert)
+    where lc = getCell i (j - 1) m -- left cell score
+          dc = getCell (i - 1) (j - 1) m -- diagonal cell score
+          uc = getCell (i - 1) j m -- up cell score
+          lcScore = snd lc
+          dcScore = snd dc
+          ucScore = snd uc
+          match = dcScore + (matchScore b1 b2)
+          delete = ucScore + gapPenalty
+          insert = lcScore + gapPenalty
+          b1 = s1 !! trace ("\nSCORE CELL\ti: " ++ show i ++ "\tj: " ++ show j) i -- sequence 1 base
+          b2 = s2 !! j -- sequence 2 base
+
+gapPenalty :: Int
+gapPenalty = -2
+
+matchScore :: Char -> Char -> Int
+matchScore b1 b2
+    | b1 == b2 = 1
+    | otherwise = -1
+
 smithWaterman :: String -> String -> [String]
-smithWaterman seq1 seq2 = []
+smithWaterman seq1 seq2 = getResult (mapMatrix scoreCell seq1 seq2 (emptyMatrix (length seq1) (length seq2))) seq1 seq2
 
 
 -- Main function
@@ -47,4 +99,8 @@ main = do fromHandle <- openFile "test_input.txt" ReadMode
           let result = smithWaterman seq1 seq2
           putStrLn "\n## Local Aligner"
           putStrLn contents
+          putStrLn ""
+          putStrLn ""
+          putStrLn (result !! 0)
+          putStrLn (result !! 1)
 
